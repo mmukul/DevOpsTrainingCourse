0bdc52e5c061882e6f5fc3745c2a30843cac091f
Mukul Malhotra <smilemukul2005@gmail.com>
2022-11-17 13:13:37 +0530

    fixed

--- bash-script-examples/scripts/evenodd.sh
+++ bash-script-examples/scripts/evenodd.sh
@@ -2,7 +2,7 @@
 echo -n "Enter The Number: "
 read -r n
 if [ $((n % 2)) -eq 0 ]; then
-	echo "is a Even Number"
+	echo "Number is a Even Number"
 else
-	echo "is a Odd Number"
+	echo "Number is a odd Number"
 fi
