a570773e2a1760769e4ebfab7ca7bb909c059077
Mukul Malhotra <smilemukul2005@gmail.com>
2024-01-20 15:47:57 +0530

    Delete terraform/3-tier-architecture directory

--- terraform/3-tier-architecture/sample_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-yum update -y && yum install -y httpd.x86_64
-systemctl start httpd.service && systemctl enable httpd.service
-echo "3-tier Application Project" > /var/www/html/index.html
